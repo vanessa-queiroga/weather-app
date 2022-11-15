e0cdb83b6c9f6de17973382f5b35b07571606e92
vanessa-queiroga <116520236+vanessa-queiroga@users.noreply.github.com>
2022-11-15 21:06:32 +0000

    Added function - Barcelona real-time weather forecast on load as default

--- src/index.js
+++ src/index.js
@@ -82,3 +82,12 @@ function getCurrentLocation(event) {
 
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", getCurrentLocation);
+
+function searchCity() {
+  let city = "Barcelona";
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+searchCity();
